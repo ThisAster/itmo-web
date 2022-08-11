92e6c866255654b679c6cc0c0b29ea759dbd3a6e
ThisAster <freizen034@gmail.com>
2022-08-11 20:46:03 +0300

    add show params

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-function onClick() {
-    alert('Hi');
+function checkPoint() {
+    const data = getData();
+    alert(JSON.stringify(data));
+}
+
+function getData() {
+    const xInput = document.getElementById("x"); 
+    const ySelect = document.getElementById("y");
+    const rSelect = document.getElementById("r");
+    const ySelectedOption = ySelect.options[ySelect.selectedIndex];
+    const rSelectedOption = rSelect.options[rSelect.selectedIndex];
+    const data = {
+        x: xInput.value,
+        y: ySelectedOption.text,
+        r: rSelectedOption.text
+    }
+    return data;
 }
