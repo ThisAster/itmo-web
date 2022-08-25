2f9776831c992334c0043d8510ea9fef707d93b2
ThisAster <freizen034@gmail.com>
2022-08-26 02:04:07 +0300

    edit

--- index.php
+++ index.php
@@ -9,6 +9,8 @@
   <link rel="stylesheet" href="style.css" />
   <script src="grapher.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/superagent@8.0.0/dist/superagent.min.js"></script>
+  <!-- JavaScript Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   <script src="index.js"></script>
 </head>
 
