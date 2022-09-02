2060cc5b0b6040694d339fc643934b5df390d6ac
ThisAster <freizen034@gmail.com>
2022-09-02 21:23:35 +0300

    added scss

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ session_start();
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Лабораторная работа №1 по веб-программированию</title>
-  <link rel="stylesheet" href="style.scss" />
+  <link rel="stylesheet" href="style.css" />
   <script src="grapher.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/superagent@8.0.0/dist/superagent.min.js"></script>
   <!-- JavaScript Bundle with Popper -->
